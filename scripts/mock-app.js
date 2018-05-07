4ebfbd21e00c18d8a95e489892029fde037c0865
Ricardo Borges <borges.ricardo77@gmail.com>
2018-05-07 21:10:12 +0100

    Github repo init

---
+++ scripts/mock-app.js
@@ -0,0 +1,52 @@
+﻿'use strict'
+
+let checkServerHealthURI = "https://private-anon-14dd947258-blissrecruitmentapi.apiary-mock.com/health";
+let loadingGIF = $('#loading-block');
+let serverHealthPositive = $('#server-health-positive');
+let serverHealthNegative = $('#server-health-negative');
+let retryButton = $('#retry-button');
+
+$(document).ready(function () {    
+    hideServerHealthInfo(serverHealthPositive, 'false');
+    hideServerHealthInfo(serverHealthNegative, 'true');
+    checkServerHealth("GET", checkServerHealthURI);
+    retryButton.click(function(){
+        retryCheckingForServerHealth();
+    });
+});
+
+function checkServerHealth(method, uri) {
+    $.ajax({
+        crossDomain: true,
+        dataType: method,
+        url: uri,
+        cache: false,
+        success: function (html) {
+            $('#server-health-positive').show();
+        },
+        error: function (html) {
+            $('#server-health-negative').show();
+        },
+        complete: function () {
+            $('#loading-block').hide();
+        }
+    });
+}
+
+function hideServerHealthInfo(serverHealthInfoToHide, alsoHideRetryButton) {  
+    serverHealthInfoToHide.hide();
+    
+    if(alsoHideRetryButton === 'true'){
+        hideRetryButton(retryButton);
+    }
+}
+
+function retryCheckingForServerHealth(){
+    loadingGIF.show();
+    hideServerHealthInfo(serverHealthNegative);
+    checkServerHealth("GET", checkServerHealthURI);
+}
+
+function hideRetryButton(retrybtnToHide){
+    retrybtnToHide.hide();
+}
